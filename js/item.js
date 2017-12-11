92b6055afd6909a6373a457dff4235fc3300b588
foo <bar@example.com>
2017-12-11 15:54:02 +0800

    Added the add element item.

--- js/item.js
+++ js/item.js
@@ -6,25 +6,59 @@ var submit = document.getElementById("add-item");
 var firebaseRef = firebase.database().ref();
 
 function addItem() {
-	// console.log("ok");
+	
 	var itemsRef = firebase.database().ref("Items/");
 	
 	var itemNameVal = itemName.value;
 	var itemPriceVal = itemPrice.value;
 	var itemCatVal = itemCat.value;
-	// var itemDesVal = itemDesVal.value;
+	var itemDesVal = itemDes.value;
 
 	itemsRef.push({
 		name: itemNameVal,
 		price: itemPriceVal,
 		category: itemCatVal,
-		// description: itemDesVal
+		description: itemDesVal
 	});
-	console.log("ok");
+
+	var container = document.getElementById("food-item-list");
+
+	var item = document.createElement("li");
+	item.className = "item-box";
+
+	var name = document.createElement("span");
+	name.className = "item-name";
+	name.innerHTML = itemNameVal;
+
+	var price = document.createElement("span");
+	price.className = "item-price";
+	price.innerHTML = itemPriceVal;
+
+	var category = document.createElement("span");
+	category.className = "item-category";
+	category.innerHTML = itemCatVal;
+
+	var description = document.createElement("span");
+	description.className = "item-description";
+	description.innerHTML = itemDesVal; 
+
+	item.appendChild(name);
+	item.appendChild(price);
+	item.appendChild(category);
+	item.appendChild(description);
+	container.appendChild(item);
+
+	console.log("Created " + itemNameVal + ".");
 }
 
 firebaseRef.on("value", function(snapshot){
 	console.log(snapshot.val());
+
+	var items = snapshot;
+	
+	// for (var i = 0; i < items.length; i++) {
+		console.log(items.child("name"));
+	// }
 }, function (error){
 	console.log("Error: " + error.code);
 });
